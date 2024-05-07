0be4fb102d287dff8f700081854c6895ac8f22df
Ana Brunetto <159146659+anabrunetto@users.noreply.github.com>
2024-05-06 23:47:15 -0300

    Add files via upload

---
+++ Questao numero 1.py
@@ -0,0 +1,7 @@
+def imprimir_info(nome, idade, cidade):
+    print("Nome:", nome, end=" - ")
+    print("Idade:", idade, end=" - ")
+    print("Cidade:", cidade + "!", sep=" ")
+
+# Exemplo de uso:
+imprimir_info("Ana Brunetto", 20, "Rio de Janeiro")
